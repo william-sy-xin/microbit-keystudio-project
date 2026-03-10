ee829ea1828b11fea701bba15d05e5b2168cba6e
William Xin <129995446+william-sy-xin@users.noreply.github.com>
2026-03-10 16:05:47 -0700

    added prj4

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("\"A\"")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("\"AB\"")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("\"B\"")
+})
+basic.forever(function () {
+	
+})
